17f79d55ff561b7922297119b4fe00516da96c7b
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2018-02-13 16:05:17 +0100

    land: Build oss consumerir hal

--- product/consumerir.mk
+++ product/consumerir.mk
@@ -1,3 +1,7 @@
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
+
+# Consumerir
+PRODUCT_PACKAGES += \
+    consumerir.msm8937
